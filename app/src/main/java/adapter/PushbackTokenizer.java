f26d63b548f73bac1a3bb620f43bb2f33fc80c56
Tyler Kelley <Kelltyl2@isu.edu>
2021-10-15 15:21:10 -0600

    added the test package

--- app/src/main/java/adapter/PushbackTokenizer.java
+++ app/src/main/java/adapter/PushbackTokenizer.java
@@ -1,5 +1,14 @@
-/*package adapter;
+/**
+ * @author Tyler Kelley
+ * @author CS2263 Homework 2 Isaac Griffith
+ * @version 2.1.0
+ * @since 1.0.0
+ */
+package adapter;
 
+/**
+ * Pushbacktokenizer class used for implementing tests on methods
+ */
 public interface PushbackTokenizer {
     //returns the next token
     String nextToken();
@@ -8,4 +17,3 @@ public interface PushbackTokenizer {
     //the token read is pushed back so it can be read again using nextToken
     void pushBack();
 }
-*/
---
+++ app/src/test/java/adapterTest/pushbackTokenizerTest.java
@@ -0,0 +1,4 @@
+package adapterTest;
+
+public class pushbackTokenizerTest {
+}
